60b9446b43d57038d5831ef75c5fe596e687491b
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-07-26 19:18:59 +0200

    fix: app build

--- vite.config.ts
+++ vite.config.ts
@@ -8,45 +8,45 @@ import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 const config = defineConfig({
+  base: process.env.argv?.includes("--app") ? "/base/" : "/",
   plugins: [
     react(),
     libInjectCss(),
     dts({ include: ["src", "lib"], tsconfigPath: "./tsconfig.build.json" }),
   ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts"],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-      output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+  build: process.env.argv?.includes("--app")
+    ? undefined
+    : {
+        copyPublicDir: false,
+        lib: {
+          entry: resolve(__dirname, "lib/main.ts"),
+          formats: ["es"],
+        },
+        rollupOptions: {
+          external: ["react", "react/jsx-runtime"],
+          input: Object.fromEntries(
+            glob
+              .sync("lib/**/*.{ts,tsx}", {
+                ignore: ["lib/**/*.d.ts"],
+              })
+              .map((file) => [
+                // The name of the entry point
+                // lib/nested/foo.ts becomes nested/foo
+                relative(
+                  "lib",
+                  file.slice(0, file.length - extname(file).length)
+                ),
+                // The absolute path to the entry file
+                // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                fileURLToPath(new URL(file, import.meta.url)),
+              ])
+          ),
+          output: {
+            assetFileNames: "assets/[name][extname]",
+            entryFileNames: "[name].js",
+          },
+        },
       },
-    },
-  },
 });
-
-if (process.argv.includes("--app")) {
-  // Remove the console logs in production builds
-  delete config.build;
-  config.base = "/base/";
-}
 
 export default config;
